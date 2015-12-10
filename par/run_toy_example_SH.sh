1b501c5da59e63ae3590f2ca9ff45d6250e794e0
Florian Wittkamp <florian.wittkamp@student.kit.edu>
2015-12-10 12:50:49 +0100

    Approx Hessian: Changed scaling factor & minor changes

--- par/run_toy_example_SH.sh
+++ par/run_toy_example_SH.sh
@@ -5,25 +5,23 @@
 ###############################################################
 
 # compiling all libraries and  DENISE
-# make clean
+make clean
 make denise MODEL=../genmod/toy_example_true.c
-pause
+
 # starting DENISE for forward modeling
 # (depending on the MPI package you maybe have to adjust the following programme call, 
 #  e.g. when using openMPI you do not have to use the 'lamboot' command)
 #lamboot
-mpirun -np 4 ../bin/denise in_and_out/toy_example/toy_example_FW.json | tee in_and_out/toy_example/toy_example_FW.out
+mpirun -np 4 ../bin/denise in_and_out/toy_example/toy_example_FW_SH.json | tee in_and_out/toy_example/toy_example_FW_SH.out
 
 # the forward modeled data have to be renamed for the inversion
 for (( i=1; i <= 5; i++ )) ; do
-
      mv su/measured_data/toy_example_vx.su.shot${i}.it1 su/measured_data/toy_example_x.su.shot${i}
      mv su/measured_data/toy_example_vy.su.shot${i}.it1 su/measured_data/toy_example_y.su.shot${i}
      mv su/measured_data/toy_example_vz.su.shot${i}.it1 su/measured_data/toy_example_z.su.shot${i}
 
 done
 
-exit
 
 ###############################################################
 #                    running the inversion                    #
