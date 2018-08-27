40088b1cdfee717763a50f2cd1c1a8893fce5702
Jeremy Kemp <jakemp@uh.edu>
2018-08-27 18:57:38 -0400

    starting new project directories

---
+++ runs/scaling-2-2/toy_scaling-2-2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+###############################################################
+#  running forward modeling for calculation of observed data  #
+###############################################################
+
+rm model_true/mod_toy_example*
+rm model/mod_toy_example*
+rm model/toy_example/mod_toy_example*
+rm jacobian/toy_example/jac_toy_example*
+rm su/toy_example/toy_example*
+rm su/measured_data/toy_example*
+
+# compiling all libraries and  IFOS
+make install MODEL_AC=../genmod/toy_example_ac_true.c
+
+# starting IFOS for forward modeling
+mpirun -np 4 nice -19 ../bin/IFOS2D in_and_out/toy_scaling/FW_2_2.json
+
+###############################################################
+#                    running the inversion                    #
+###############################################################
+
+# compiling IFOS
+make install MODEL_AC=../genmod/toy_example_ac_start.c
+
+# starting IFOS
+mpirun -np 4 nice -19 ../bin/IFOS2D in_and_out/toy_scaling/FW_2_2.json
+
+make clean
+
+rm jacobian/toy_example/*toy_example*.*.*.*
+rm model/*.bin.*.*
+rm model/toy_example/*.bin.*.*
+rm *.bin.*.*
