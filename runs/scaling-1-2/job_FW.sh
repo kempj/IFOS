75e085b7c7d2781b3d607075dfa341801dc9ac01
Jeremy Kemp <jakemp@uh.edu>
2018-08-29 00:08:35 -0400

    adding smaller jobs

---
+++ runs/scaling-1-2/job_FW.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J run-FW-IFOS      # job name
+#SBATCH -o run-FW-IFOS.o%j  # output and error file name (%j expands to jobID)
+#SBATCH -N 1                    # total number of nodes requested
+#SBATCH --ntasks-per-node 2    # total number of processors on each node
+#SBATCH --mem 16000              # total memory request in MB
+#SBATCH -p compute                  # queue (partition)
+#SBATCH -t 30:00:00             # run time (hh:mm:ss)
+#SBATCH --mail-user=jekemp@pvamu.edu  # change to your email address
+#SBATCH --mail-type=begin       # email me when the job starts
+#SBATCH --mail-type=end         # email me when the job finishes
+
+./run_FW.sh
+
