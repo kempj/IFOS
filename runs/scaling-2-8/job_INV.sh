4ae6faed754cf8c42e1b7c1423bf7f7e947933c2
Jeremy Kemp <jakemp@uh.edu>
2018-08-29 01:42:14 -0400

    added exclusive

--- runs/scaling-2-8/job_INV.sh
+++ runs/scaling-2-8/job_INV.sh
@@ -6,6 +6,7 @@
 #SBATCH --mem 16000              # total memory request in MB
 #SBATCH -p compute                  # queue (partition)
 #SBATCH -t 30:00:00             # run time (hh:mm:ss)
+#SBATCH --exclusive
 #SBATCH --mail-user=jekemp@pvamu.edu  # change to your email address
 #SBATCH --mail-type=begin       # email me when the job starts
 #SBATCH --mail-type=end         # email me when the job finishes
